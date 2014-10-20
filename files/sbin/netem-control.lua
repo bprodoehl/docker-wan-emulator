4939bcf53812ec8224783a0abb266f98d9fba854
Brian Prodoehl <bprodoehl@connectify.me>
2014-10-20 12:29:30 -0400

    Add delay distribution control.

--- files/sbin/netem-control.lua
+++ files/sbin/netem-control.lua
@@ -78,7 +78,11 @@ function setRules (operation)
                 if delay_corr == nil then
                     delay_corr = 0
                 end
-                tc_str = tc_str.." delay "..delay_ms.."ms "..delay_var.."ms "..delay_corr.."%"
+		        delay_dist = x:get("netem", section[".name"], "delay_dist")
+                if delay_dist == nil then
+                    delay_dist = "normal"
+                end
+                tc_str = tc_str.." delay "..delay_ms.."ms "..delay_var.."ms "..delay_corr.."% distribution "..delay_dist
             end
 
             reorder_enabled = x:get("netem", section[".name"], "reordering")
